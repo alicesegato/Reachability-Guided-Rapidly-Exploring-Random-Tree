54ada5d2bdb76b417a4397f74f85086df1d98540
Fasai <fp10@rice.edu>
2018-10-13 22:36:43 -0500

    added some stuff to Project4Car.cpp

--- src/Project4Car.cpp
+++ src/Project4Car.cpp
@@ -7,9 +7,15 @@
 #include <iostream>
 
 #include <ompl/base/ProjectionEvaluator.h>
+#include <ompl/base/spaces/SE2StateSpace.h>
 
 #include <ompl/control/SimpleSetup.h>
 #include <ompl/control/ODESolver.h>
+#include <ompl/control/spaces/RealVectorControlSpace.h>
+#include <ompl/control/planners/rrt/RRT.h>
+#include <ompl/control/planners/kpiece/KPIECE1.h>
+
+#include <ompl/tools/benchmark/Benchmark.h>
 
 // The collision checker produced in project 2
 #include "CollisionChecking.h"
@@ -37,32 +43,113 @@ public:
     }
 };
 
-void carODE(const ompl::control::ODESolver::StateType & /* q */, const ompl::control::Control * /* control */,
-            ompl::control::ODESolver::StateType & /* qdot */)
+void carODE(const ompl::control::ODESolver::StateType & q, const ompl::control::Control * control,
+            ompl::control::ODESolver::StateType & qdot)
 {
-    // TODO: Fill in the ODE for the car's dynamics
+    // Retrieve control values.  Velocity is the first entry, steering angle is second.
+    const double *u = control->as<ompl::control::RealVectorControlSpace::ControlType>()->values;
+    const double velocity = u[0];
+    const double steeringAngle = u[1];
+
+    // Retrieve the current orientation of the car.  The memory for ompl::base::SE2StateSpace is mapped as:
+    // 0: x
+    // 1: y
+    // 2: theta
+    const double theta = q[2];
+
+    // Ensure qdot is the same size as q.  Zero out all values.
+    qdot.resize(q.size(), 0);
+
+    qdot[0] = velocity * cos(theta);            // x-dot
+    qdot[1] = velocity * sin(theta);            // y-dot
+    qdot[2] = velocity * tan(steeringAngle);    // theta-dot
 }
 
-void makeStreet(std::vector<Rectangle> & /* obstacles */)
+void makeStreet(std::vector<Rectangle> & obstacles)
 {
     // TODO: Fill in the vector of rectangles with your street environment.
+    Rectangle rect;
+
+    rect.x = 3.0;
+    rect.y = 3.0;
+    rect.width = 2.0;
+    rect.height - 5.0;
+    obstacles.push_back(rect);
+
+    rect.x = 7.0;
+    rect.y = 7.0;
+    rect.width = 1.0;
+    rect.height = 1.0;
+    obstacles.push_back(rect);
 }
 
-ompl::control::SimpleSetupPtr createCar(std::vector<Rectangle> & /* obstacles */)
+ompl::control::SimpleSetupPtr createCar(std::vector<Rectangle> & obstacles)
 {
     // TODO: Create and setup the car's state space, control space, validity checker, everything you need for planning.
-    return nullptr;
+    // state space
+    auto se2 = std::make_shared<ompl::base::SE2StateSpace>();
+    ompl::base::RealVectorBounds bounds(2);
+    bounds.setLow(0);
+    bounds.setHigh(10);
+    se2->setBounds(bounds);
+
+    // control space
+    auto cspace(std::make_shared<ompl::control::RealVectorControlSpace>(se2, 2));
+    ompl::base::RealVectorBounds cbounds(2);
+    bounds.setLow(-0.3);
+    bounds.setHigh(0.3);
+    cspace->setBounds(cbounds);
+
+    // Simple Setup
+    ompl::control::SimpleSetupPtr ss(new ompl::control::SimpleSetup(cspace));
+
+    // Propagation
+//    ss->setStatePropagator(propagate);
+
+    // Validity checker
+//    ss->setStateValidityChecker(std::bind(isValidStateSquare, std::placeholders::_1, sideLength, obstacles));
+
+    // start state
+    ompl::base::ScopedState<> start(se2);
+    // goal state
+    ompl::base::ScopedState<> goal(se2);
+    ss->setStartAndGoalStates(start, goal);
+
+    return ss;
 }
 
-void planCar(ompl::control::SimpleSetupPtr &/* ss */, int /* choice */)
+void planCar(ompl::control::SimpleSetupPtr & ss, int choice)
 {
     // TODO: Do some motion planning for the car
     // choice is what planner to use.
+    if (choice == 1)
+    {
+        //RRT
+        ss->setPlanner(std::make_shared<ompl::control::RRT>(ss->getSpaceInformation()));
+    }
+    else if (choice == 2)
+    {
+        //KPIECE1
+        ss->setPlanner(std::make_shared<ompl::control::KPIECE1>(ss->getSpaceInformation()));
+    }
+    else if (choice == 3)
+    {
+        //RG-RRT
+    }
 }
 
-void benchmarkCar(ompl::control::SimpleSetupPtr &/* ss */)
+void benchmarkCar(ompl::control::SimpleSetupPtr & ss)
 {
     // TODO: Do some benchmarking for the car
+    ompl::tools::Benchmark::Request request(100., 10000., 10);  // runtime (s), memory (MB), run count
+
+//    ss->setup();
+//
+//    ompl::tools::Benchmark b(ss, ss.getName());
+//    b.addPlanner(std::make_shared<ompl::control::RRT>(ss->getSpaceInformation()));
+//    b.addPlanner(std::make_shared<ompl::control::KPIECE1>(ss->getSpaceInformation()));
+//    b.benchmark(request);
+//    b.saveResultsToFile();
 }
 
 int main(int /* argc */, char ** /* argv */)
